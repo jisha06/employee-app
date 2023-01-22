12c1d01092a440417106fc83cc5614957a8e6652
jisha06 <116796524+jisha06@users.noreply.github.com>
2023-01-22 18:23:35 +0530

    employee

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Employee from './components/Employee';
+import AddEmployee from './components/AddEmployee';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element ={<Employee/>} ></Route>
+          <Route path='/addemployee' element={<AddEmployee/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
